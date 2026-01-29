8019c4cbedea7712139f45b3f386e1021cdba450
lyk3918 <lyk3918@gmail.com>
2026-01-29 16:47:02 +0900

    add ignore hello

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main (void){
+
+
+print("hello world");
+return 0;
+}
---
+++ print.h
@@ -0,0 +1,5 @@
+#ifndef _PRINT_H_
+#define _PRINT_H_
+extern void print (const char*);
+
+#endif //_PRINT_H_
